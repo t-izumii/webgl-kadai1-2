c5717922467af91fe1cf50606f04b63207c12a3b
t-izumii <120708670+t-izumii@users.noreply.github.com>
2023-06-01 17:14:56 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,231 @@
+
+// = 009 ======================================================================
+// これまでのサンプルでは、メッシュは「１つのジオメトリから１つ」ずつ生成してい
+// ましたが、実際の案件では、同じジオメトリを再利用しながら「複数のメッシュ」を
+// 生成する場面のほうが多いかもしれません。
+// このとき、3D シーンに複数のオブジェクトを追加する際にやってしまいがちな間違い
+// として「ジオメトリやマテリアルも複数回生成してしまう」というものがあります。
+// メモリ効率よく複数のオブジェクトをシーンに追加する方法をしっかりおさえておき
+// ましょう。
+// ============================================================================
+
+// 必要なモジュールを読み込み
+import * as THREE from '../lib/three.module.js';
+import { OrbitControls } from '../lib/OrbitControls.js';
+
+// DOM がパースされたことを検出するイベントを設定
+window.addEventListener('DOMContentLoaded', () => {
+  // 制御クラスのインスタンスを生成
+  const app = new App3();
+  // 初期化
+  app.init();
+  // 描画
+  app.render();
+}, false);
+
+/**
+ * three.js を効率よく扱うために自家製の制御クラスを定義
+ */
+class App3 {
+  /**
+   * カメラ定義のための定数
+   */
+  static get CAMERA_PARAM() {
+    return {
+      // fovy は Field of View Y のことで、縦方向の視野角を意味する
+      fovy: 60,
+      // 描画する空間のアスペクト比（縦横比）
+      aspect: window.innerWidth / window.innerHeight,
+      // 描画する空間のニアクリップ面（最近面）
+      near: 0.1,
+      // 描画する空間のファークリップ面（最遠面）
+      far: 2000.0,
+      // カメラの位置
+      x: 0.0,
+      y: 0.0,
+      z: 800.0,
+      // カメラの中止点
+      lookAt: new THREE.Vector3(0.0, 0.0, 0.0),
+    };
+  }
+  /**
+   * レンダラー定義のための定数
+   */
+  static get RENDERER_PARAM() {
+    return {
+      // レンダラーが背景をリセットする際に使われる背景色
+      clearColor: 0x666666,
+      // レンダラーが描画する領域の横幅
+      width: window.innerWidth,
+      // レンダラーが描画する領域の縦幅
+      height: window.innerHeight,
+    };
+  }
+  /**
+   * ディレクショナルライト定義のための定数
+   */
+  static get DIRECTIONAL_LIGHT_PARAM() {
+    return {
+      color: 0xffffff, // 光の色
+      intensity: 1.0,  // 光の強度
+      x: 1.0,          // 光の向きを表すベクトルの X 要素
+      y: 1.0,          // 光の向きを表すベクトルの Y 要素
+      z: 1.0           // 光の向きを表すベクトルの Z 要素
+    };
+  }
+  /**
+   * アンビエントライト定義のための定数
+   */
+  static get AMBIENT_LIGHT_PARAM() {
+    return {
+      color: 0xffffff, // 光の色
+      intensity: 0.2,  // 光の強度
+    };
+  }
+  /**
+   * マテリアル定義のための定数
+   */
+  static get MATERIAL_PARAM() {
+    return {
+      color: 0xffffff, // マテリアルの基本色
+    };
+  }
+  static get LINE_MATERIAL_PARAM() {
+    return {
+      color: 0x000000, // マテリアルの基本色
+      transparent: true,
+      opacity: 0.0,
+    };
+  }
+
+  /**
+   * コンストラクタ
+   * @constructor
+   */
+  constructor() {
+    this.renderer;         // レンダラ
+    this.scene;            // シーン
+    this.camera;           // カメラ
+    this.directionalLight; // ディレクショナルライト
+    this.ambientLight;     // アンビエントライト
+    this.material;         // マテリアル
+    this.boxGeometry       // ボックスジオメトリ
+    this.box               // メッシュ
+    this.controls;         // オービットコントロール
+    this.axesHelper;       // 軸ヘルパー
+
+    this.isDown = false; // キーの押下状態を保持するフラグ
+
+    // 再帰呼び出しのための this 固定
+    this.render = this.render.bind(this);
+
+    // リサイズイベント
+    window.addEventListener('resize', () => {
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
+    }, false);
+  }
+
+  /**
+   * 初期化処理
+   */
+  init() {
+    // レンダラー
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setClearColor(new THREE.Color(App3.RENDERER_PARAM.clearColor));
+    this.renderer.setSize(App3.RENDERER_PARAM.width, App3.RENDERER_PARAM.height);
+    const wrapper = document.querySelector('#webgl');
+    wrapper.appendChild(this.renderer.domElement);
+
+    // シーン
+    this.scene = new THREE.Scene();
+
+    // カメラ
+    this.camera = new THREE.PerspectiveCamera(
+      App3.CAMERA_PARAM.fovy,
+      App3.CAMERA_PARAM.aspect,
+      App3.CAMERA_PARAM.near,
+      App3.CAMERA_PARAM.far,
+    );
+    this.camera.position.set(
+      App3.CAMERA_PARAM.x,
+      App3.CAMERA_PARAM.y,
+      App3.CAMERA_PARAM.z,
+    );
+    this.camera.lookAt(App3.CAMERA_PARAM.lookAt);
+
+    // ディレクショナルライト（平行光源）
+    this.directionalLight = new THREE.DirectionalLight(
+      App3.DIRECTIONAL_LIGHT_PARAM.color,
+      App3.DIRECTIONAL_LIGHT_PARAM.intensity
+    );
+    this.directionalLight.position.set(
+      App3.DIRECTIONAL_LIGHT_PARAM.x,
+      App3.DIRECTIONAL_LIGHT_PARAM.y,
+      App3.DIRECTIONAL_LIGHT_PARAM.z,
+    );
+    this.scene.add(this.directionalLight);
+
+    // アンビエントライト（環境光）
+    this.ambientLight = new THREE.AmbientLight(
+      App3.AMBIENT_LIGHT_PARAM.color,
+      App3.AMBIENT_LIGHT_PARAM.intensity,
+    );
+    this.scene.add(this.ambientLight);
+
+
+   // マテリアル
+  const BOX_ROW = 448;
+  const BOX_COL = 602;
+  const BOX_SIZE = 1.0;
+  const gridWidth = BOX_ROW * BOX_SIZE;
+  const gridHeight = BOX_COL * BOX_SIZE;
+  const offsetX = -gridWidth / 2;
+  const offsetY = -gridHeight / 2;
+
+  // Three.jsの初期化などのコード
+
+  // 外部ファイルからJSONデータを読み込む
+  fetch('output.json')
+    .then(response => response.json())
+    .then(jsonData => {
+      // JSONデータからボックスを作成
+      jsonData.forEach(data => {
+        const { x, y, r, g, b } = data;
+        const boxGeometry = new THREE.BoxGeometry(BOX_SIZE, BOX_SIZE, BOX_SIZE);
+        const material = new THREE.MeshPhongMaterial({ color: `rgb(${r}, ${g}, ${b})` });
+        const box = new THREE.Mesh(boxGeometry, material);
+        box.position.set(x * BOX_SIZE + offsetX, -y * BOX_SIZE - offsetY, 0);
+        this.scene.add(box);
+      });
+    })
+    .catch(error => {
+      console.error('Error loading JSON:', error);
+    });
+
+
+    // コントロール
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    // ヘルパー
+    // const axesBarLength = 5.0;
+    // this.axesHelper = new THREE.AxesHelper(axesBarLength);
+    // this.scene.add(this.axesHelper);
+  }
+
+  /**
+   * 描画処理
+   */
+  render() {
+    // 恒常ループの設定
+    requestAnimationFrame(this.render);
+
+    // コントロールを更新
+    this.controls.update();
+
+    // レンダラーで描画
+    this.renderer.render(this.scene, this.camera);
+  }
+}
+
